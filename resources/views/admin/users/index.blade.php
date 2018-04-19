08dd147a428bb47d9170475cdfca1ca152c5faf8
Bairei Kawagishi <baireikawagishi@gmail.com>
2018-04-19 19:08:00 +0200

    Add creating users and displaying their list, add Photo model

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,48 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 
-    <div class="alert alert-success">Users index page has been opened successfully!</div>
+    <h1>Users</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Status</th>
+                <th>Created</th>
+                <th>Updated</th>
+            </tr>
+        </thead>
+        <tbody>
+        @if($users)
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->id}}</td>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    @if($user->role)
+                        <td>{{$user->role->name}}</td>
+                    @else
+                        <td>None</td>
+                    @endif
+                    <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+                    @if($user->created_at)
+                        <td>{{$user->created_at->diffForHumans()}}</td>
+                    @else
+                        <td>--</td>
+                    @endif
+                    @if($user->updated_at)
+                        <td>{{$user->updated_at->diffForHumans()}}</td>
+                    @else
+                        <td>--</td>
+                    @endif
+                </tr>
+            @endforeach
+        @endif
+        </tbody>
+    </table>
 
 @endsection
